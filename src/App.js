a876c2b556242e291c68584729332b9e8c125c13
mohdShahwan <mshahwan002@gmail.com>
2023-06-13 08:44:57 +0300

    Initial Commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import { useState } from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <Counter />
+    </div>
+  );
+}
+
+function Counter() {
+  const [step, setStep] = useState(1);
+  const [count, setCount] = useState(0);
+
+  const date = new Date();
+  date.setDate(date.getDate() + count);
+
+  function handleReset() {
+    setCount(0);
+    setStep(1);
+  }
+
+  return (
+    <>
+      <div>
+        <input
+          onChange={(e) => setStep((s) => +e.target.value)}
+          value={step}
+          type="range"
+          min="1"
+          max="10"
+        />
+        {step}
+      </div>
+      <div>
+        <button onClick={() => setCount((c) => c - step)}>-</button>
+        <input
+          type="text"
+          value={count}
+          onChange={(e) => setCount(() => +e.target.value)}
+        />
+        <button onClick={() => setCount((c) => c + step)}>+</button>
+      </div>
+      <p>
+        <span>
+          {count === 0
+            ? "Today is "
+            : count > 0
+            ? `${count} days from today is `
+            : `${count * -1} days ago was `}
+        </span>
+        <span>{date.toDateString()}</span>
+      </p>
+      {count !== 0 || step !== 1 ? (
+        <div>
+          <button onClick={handleReset}>Reset</button>
+        </div>
+      ) : null}
+    </>
+  );
+}
